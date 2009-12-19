4c5079066c7ec9624db473016ad252b295dbcb03
Justin Quick <justquick@gmail.com>
2009-12-19 06:20:46 -0500

    moved benchmarks around a bit

--- test_project/benchmarks/graph.py
+++ test_project/benchmarks/graph.py
@@ -1,4 +1,6 @@
 from glob import glob
+from GChartWrapper import VerticalBarGroup
+
 
 data = {}
 
@@ -56,9 +58,7 @@ for bench in glob('*.txt'):
     d['responses'] = int(rest[-1].strip().split()[-1])
     f.close()
 
-from pprint import pprint
 
-from GChartWrapper import *
 
 def graph_factory(name, scale=(0,11000), symbol='f'):
     values = [(k, x[name]) for k,x in data.items()]
@@ -75,40 +75,12 @@ def graph_factory(name, scale=(0,11000), symbol='f'):
     for i in range(4):
         G.marker('N*%s*'%symbol,'black',i,1,11)
     G.save('graphs/%s'%name)
-    print G
     
 lookup2 = {
     'responses':'Total Responses Returned',
-    'bytes':'Total Bytes Returned',
     'fetches_per_sec': 'Page Fetches per Second',
-    'bad_byte_counts': 'Number of Pages with Bad Byte Counts',
     'mean_first_response': 'Average Time Until Frist Response',
-    'mean_per_conn': 'Average Time Until First Connection'
 }
 graph_factory('responses', (0,60000))
-#graph_factory('bytes', (10**6, 10**7.2), 'e')
 graph_factory('fetches_per_sec', (0,900))
-#graph_factory('bad_byte_counts')
 graph_factory('mean_first_response',(0,120))
-#graph_factory('mean_per_conn', (0.5, .1))
-
-"""
-10224 fetches, 20 max parallel, 1.37046e+07 bytes, in 60 seconds
-1340.43 mean bytes/connection
-170.4 fetches/sec, 228409 bytes/sec
-msecs/connect: 74.7478 mean, 7007.02 max, 0.05 min
-msecs/first-response: 30.9129 mean, 18919 max, 0.036 min
-9810 bad byte counts
-HTTP response codes:
-  code 500 -- 9810
-
-9193 fetches, 20 max parallel, 1.22573e+07 bytes, in 60 seconds
-1333.33 mean bytes/connection
-153.217 fetches/sec, 204288 bytes/sec
-msecs/connect: 83.7515 mean, 7007.3 max, 0.041 min
-msecs/first-response: 36.2251 mean, 18918.6 max, 0.035 min
-419 bad byte counts
-HTTP response codes:
-  code 500 -- 8774
-
-"""
